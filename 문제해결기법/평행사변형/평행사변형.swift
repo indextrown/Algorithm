1518bd3894df53bca4e04117b0b91302cb7ae7da
indextrown <lghan00020@nate.com>
2024-07-20 14:16:51 +0900

    push

---
+++ 문제해결기법/평행사변형/평행사변형.swift
@@ -0,0 +1,15 @@
+//
+//  평행사변형.swift
+//  Algorithm
+//
+//  Created by 김동현 on 7/19/24.
+//
+
+import Foundation
+
+@main
+struct Main {
+    static func main() {
+        print("test")
+    }
+}
