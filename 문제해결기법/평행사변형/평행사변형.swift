f0c8d36c91b63d60f1db6cfb0daaf56b96f8541f
indextrown <lghan00020@nate.com>
2024-07-20 14:29:26 +0900

    평행사변형.swift

--- 문제해결기법/평행사변형/평행사변형.swift
+++ 문제해결기법/평행사변형/평행사변형.swift
@@ -7,9 +7,65 @@
 
 import Foundation
 
+
+struct Point {
+    var x: Int
+    var y: Int
+}
+
+func compare(_ a: Point, _ b: Point) -> Bool {
+    return a.x < b.x
+}
+
+func distance(_ a: Point, _ b: Point) -> Int {
+    return (b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)
+}
+
+
+
 @main
 struct Main {
     static func main() {
-        print("test")
+        let inpfile = "1.inp"
+        let outfile = "parallelogram.out"
+
+        var points: [Point] = []
+
+        do {
+            let input = try String(contentsOfFile: inpfile, encoding: .utf8)
+            let lines = input.components(separatedBy: .newlines)
+            var output = ""
+
+            for line in lines {
+                if line.isEmpty { continue }
+                let values = line.split(separator: " ").map { Int($0)! }
+                for i in 0..<4 {
+                    points.append(Point(x: values[2 * i], y: values[2 * i + 1]))
+                }
+
+                // 기저사례
+                if points.allSatisfy({ $0.x == 0 && $0.y == 0 }) { break }
+
+                // x좌표 기준 오름차순 정렬
+                points.sort(by: compare)
+
+                let d1 = distance(points[0], points[1])
+                let d2 = distance(points[2], points[3])
+                let d3 = distance(points[0], points[2])
+                let d4 = distance(points[1], points[3])
+
+                if d1 == d2 && d3 == d4 {
+                    output += "1\n"
+                } else {
+                    output += "0\n"
+                }
+
+                points.removeAll()
+            }
+
+            try output.write(toFile: outfile, atomically: true, encoding: .utf8)
+        } catch {
+            print("An error occurred: \(error)")
+        }
     }
 }
