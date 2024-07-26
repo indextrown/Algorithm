32db0ba019bd027f7c2aa2f7219e7c204b05744b
indextrown <lghan00020@nate.com>
2024-07-27 03:26:13 +0900

    update

--- Baekjoon/BFS/BFS기초/BFS 기초.cpp
+++ Baekjoon/BFS/BFS기초/BFS 기초.cpp
@@ -11,14 +11,26 @@ using namespace std;
 const int dy[] = {-1, 0, 1, 0};
 const int dx[] = {0, 1, 0, -1};
 
+const int dyy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dxx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+
 int main()
 {
     int y=0, x=0;
-    
     for (int i=0; i<4; i++)
     {
         int ny = y + dy[i];
         int nx = x + dx[i];
+        cout << ny << " : " << nx << "\n";
+    }
+    cout << "\n";
+    
+    int yy=0, xx=0;
+    for (int i=0; i<8; i++)
+    {
+        int ny = yy + dyy[i];
+        int nx = xx + dxx[i];
         cout << ny << " : " << nx << "\n";
     }
 
