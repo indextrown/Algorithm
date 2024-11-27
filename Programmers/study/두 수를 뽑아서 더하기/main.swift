1c821c5696e019c527c2c9be2d7bf4b58000eec6
indextrown <lghan00020@nate.com>
2024-11-27 22:58:10 +0900

    upload

---
+++ Programmers/study/두 수를 뽑아서 더하기/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김동현 on 11/27/24.
+//
+
+import Foundation
+
+func solution(_ numbers: [Int]) -> [Int] {
+    var set = Set<Int>()
+    for i in 0..<numbers.count {
+        for j in i+1..<numbers.count {
+            set.insert(numbers[i]+numbers[j])
+        }
+    }
+
+    return Array(set).sorted()
+}
+
+func printSolution(_ vec: [Int]) {
+    print(vec.map { String($0) }.joined(separator: " "))
+}
+
+func main() {
+    printSolution(solution([2, 1, 3, 4, 1])) // 2 3 4 5 6 7
+    printSolution(solution([5, 0, 2, 7]))    // 2 5 7 9 12
+}
+
+main()
