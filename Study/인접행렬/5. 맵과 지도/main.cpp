9a927071679a12174cadd0da7b2f3533cc389f0b
김동현 <69367698+indextrown@users.noreply.github.com>
2025-01-15 22:27:58 +0900

    맵과 방향벡터 예제

--- Study/인접행렬/5. 맵과 지도/main.cpp
+++ Study/인접행렬/5. 맵과 지도/main.cpp
@@ -8,7 +8,23 @@
 #include <iostream>
 using namespace std;
 
+// MARK: 4방향
+const int dy[] = {-1, 0, 1, 0};
+const int dx[] = {0, 1, 0, -1};
+
+// MARK: 8방향
+const int dy2[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dx2[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+// MARK: 8방향
+
 int main() {
-    cout << 123 << "\n";
+    int x = 0;
+    int y = 0;
+    for (int i=0; i<4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        cout << ny << " : " << nx << '\n';
+    }
     return 0;
 }
