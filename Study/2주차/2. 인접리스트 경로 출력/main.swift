5f7ba4ece91a6db3aaec0892026c09fc36b1e61a
indextrown <indextrown@gmail.com>
2025-02-17 15:08:49 +0900

    update

--- Study/2주차/2. 인접리스트 경로 출력/main.swift
+++ Study/2주차/2. 인접리스트 경로 출력/main.swift
@@ -20,8 +20,27 @@ adj[2].append(1)
 
 adj[3].append(0)
 
+// 1번방식
 for i in 0..<v {
+    print(i, ": ", terminator: "")
     for j in 0..<adj[i].count {
-        print(adj[i][j])
+        print(adj[i][j], terminator: " ")
     }
+    print()
 }
+
+// 2번방식
+for i in 0..<v {
+    print(i, ": ", terminator: "")
+    for there in adj[i] {
+        print(there, terminator: " ")
+    }
+    print()
+}
+
+/*
+ 0 : 1 2 3
+ 1 : 0 2
+ 2 : 0 1
+ 3 : 0
+ */
