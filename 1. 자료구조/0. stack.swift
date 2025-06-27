7889a2b71bb308dac732cf4ffe557dba312d33d8
indextrown <indextrown@gmail.com>
2025-06-28 02:43:23 +0900

    update

--- 1. 자료구조/0. stack.swift
+++ 1. 자료구조/0. stack.swift
@@ -5,6 +5,8 @@
 //  Created by 김동현 on 3/4/25.
 //
 
+import Foundation
+
 struct Stack<T> {
     private var stack: [T] = []
     
@@ -29,9 +31,196 @@ struct Stack<T> {
     }
 }
 
+struct MyStack<T> {
+    @propertyWrapper
+    struct IsNonNegative {
+        private var value: Int
+        
+        init(wrappedValue: Int) {
+            self.value = wrappedValue
+        }
+        
+        var wrappedValue: Int {
+            get { value }
+            set { value = max(newValue, 0) }
+        }
+    }
+    
+    final class _StackStorage {
+        var buffer: [T] = []
+        @IsNonNegative var count = 0
+        
+        func copy() -> _StackStorage {
+            let newStorage = _StackStorage()
+            newStorage.buffer = buffer
+            newStorage.count = count
+            return newStorage
+        }
+    }
+    
+    private var _storage = _StackStorage()
+    
+    var isEmpty: Bool {
+        _storage.buffer.isEmpty
+    }
+    
+    var peek: T? {
+        _storage.buffer.last
+    }
+    
+    var count: Int {
+        _storage.count
+    }
+    
+    mutating func push(_ value: T) {
+        ensureUnique()
+        _storage.count += 1
+        return _storage.buffer.append(value)
+    }
+    
+    mutating func pop() -> T? {
+        ensureUnique()
+        guard !_storage.buffer.isEmpty else { return nil }
+        _storage.count -= 1
+        return _storage.buffer.popLast()
+    }
+    
+    private mutating func ensureUnique() {
+        if !isKnownUniquelyReferenced(&_storage) {
+            _storage = _storage.copy()
+        }
+    }
+}
+
+public struct OptimizedStack<T> {
+    private var buffer: [T]
+    
+    public init() {
+        self.buffer = []
+    }
+    
+    public init(capacity: Int) {
+        self.buffer = []
+        self.buffer.reserveCapacity(capacity)
+    }
+    
+    public var isEmpty: Bool {
+        buffer.isEmpty
+    }
+    
+    public var count: Int {
+        buffer.count
+    }
+    
+    public var peek: T? {
+        buffer.last
+    }
+    
+    public mutating func push(_ value: T) {
+        buffer.append(value)
+    }
+    
+    public mutating func pop() -> T? {
+        buffer.popLast()
+    }
+}
+
+
+func performanceTest() {
+    let testCount = 100_000
+    
+    print("--- Custom Stack ---")
+    var customStack = Stack<Int>()
+    var start = CFAbsoluteTimeGetCurrent()
+    for i in 0..<testCount {
+        customStack.push(i)
+    }
+    var duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Push: \(String(format: "%.4f", duration))초")
+    
+    start = CFAbsoluteTimeGetCurrent()
+    while !customStack.isEmpty {
+        _ = customStack.pop()
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Pop:  \(String(format: "%.4f", duration))초")
+    
+    
+    
+    print("--- Swift Array (as Stack) ---")
+    var arrayStack: [Int] = []
+    start = CFAbsoluteTimeGetCurrent()
+    for i in 0..<testCount {
+        arrayStack.append(i)
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Append: \(String(format: "%.4f", duration))초")
+    
+    start = CFAbsoluteTimeGetCurrent()
+    while !arrayStack.isEmpty {
+        _ = arrayStack.popLast()
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - popLast: \(String(format: "%.4f", duration))초")
+    
+    // MARK: - MyStack<T>
+    print("--- MyStack ---")
+    var myStack = MyStack<Int>()
+    start = CFAbsoluteTimeGetCurrent()
+    for i in 0..<testCount {
+        myStack.push(i)
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Push: \(String(format: "%.4f", duration))초")
+    
+    start = CFAbsoluteTimeGetCurrent()
+    while !myStack.isEmpty {
+        _ = myStack.pop()
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Pop:  \(String(format: "%.4f", duration))초")
+    
+    // MARK: - MyStack<T>
+    print("--- OptimizedStack ---")
+    var optimizedStack = OptimizedStack<Int>()
+    start = CFAbsoluteTimeGetCurrent()
+    for i in 0..<testCount {
+        optimizedStack.push(i)
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Push: \(String(format: "%.4f", duration))초")
+    
+    start = CFAbsoluteTimeGetCurrent()
+    while !optimizedStack.isEmpty {
+        _ = optimizedStack.pop()
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Pop:  \(String(format: "%.4f", duration))초")
+    
+    // MARK: -
+    print("--- Swift Array (with reserveCapacity) ---")
+    var reservedArrayStack: [Int] = []
+    reservedArrayStack.reserveCapacity(testCount)
+    
+    start = CFAbsoluteTimeGetCurrent()
+    for i in 0..<testCount {
+        reservedArrayStack.append(i)
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - Append: \(String(format: "%.4f", duration))초")
+    
+    start = CFAbsoluteTimeGetCurrent()
+    while !reservedArrayStack.isEmpty {
+        _ = reservedArrayStack.popLast()
+    }
+    duration = CFAbsoluteTimeGetCurrent() - start
+    print("  - popLast: \(String(format: "%.4f", duration))초")
+}
+
 @main
 struct Main {
     static func main() {
+        /*
         var mystack = Stack<Int>()
         
         mystack.push(1)
@@ -41,5 +230,8 @@ struct Main {
         while let element = mystack.pop() {
             print(element)
         }
+         */
+        performanceTest()
     }
 }
+
---
+++ 3. Programmers/8. 올바른 괄호/main.swift
@@ -0,0 +1,81 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김동현 on 6/28/25.
+//
+
+import Foundation
+
+// MARK: - 최초 시도
+func solution(_ s:String) -> Bool
+{
+    var stack: [Character] = []
+    stack.reserveCapacity(100_000)
+    
+    for c in s {
+        
+        // MARK: - 최초 시도
+         // 만약 (라면 넣기
+         if c == "(" { stack.append(c) }
+        
+         // 만약 )라면 stack의 top이 없으면 return false
+         else if c == ")" && stack.last == nil { return false }
+        
+         // 만약 )라면 stack의 top이 (이면 stack.pop
+         else if c == ")" && stack.last! == "(" { stack.removeLast() }
+    }
+    return stack.isEmpty
+}
+
+// MARK: - 두번째 시도
+func solution2(_ s:String) -> Bool
+{
+    var stack: [Character] = []
+    stack.reserveCapacity(100_000)
+    
+    for c in s {
+    
+        
+        switch c {
+        case "(":
+            stack.append(c)
+        case ")":
+            // top이 있고 top이 ( 이면 pop
+            if let top = stack.last, top == "(" {
+                stack.removeLast()
+            // top이 없거나, top이 있으나 )이면 false
+            } else {
+                return false
+            }
+        default:
+            return false
+        }
+    }
+    return stack.isEmpty
+}
+
+// MARK: - 세번째 시도
+func solution3(_ s:String) -> Bool
+{
+    var openCnt: Int = 0, closeCnt: Int = 0
+    var answer: Bool = false
+    
+    for c in s {
+        
+        switch c {
+            case "(":
+                openCnt += 1
+            case ")":
+                closeCnt += 1
+            default:
+                break
+        }
+        
+        guard openCnt >= closeCnt else {
+            break
+        }
+    }
+    answer = openCnt == closeCnt
+    return answer
+}
