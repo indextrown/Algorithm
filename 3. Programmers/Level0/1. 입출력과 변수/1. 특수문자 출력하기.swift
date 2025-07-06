8f2c30a2271ff4161cfe0d54e6ef3c91cf05c4d1
indextrown <indextrown@gmail.com>
2025-07-06 19:34:01 +0900

    update

---
+++ 3. Programmers/Level0/1. 입출력과 변수/1. 특수문자 출력하기.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김동현 on 7/6/25.
+//
+// https://school.programmers.co.kr/learn/courses/30/lessons/181948
+/*
+ 특수문자를 출력할 수 있다
+ */
+
+import Foundation
+
+@main
+struct Main {
+    static func main() {
+        print("!@#$%^&*(\\\'\"<>?:;")
+        print(#"!@#$%^&*(\'"<>?:;"#)
+    }
+}
---
+++ 3. Programmers/Level0/1. 입출력과 변수/3. 덧셈식 출력하기.swift
@@ -0,0 +1,18 @@
+//
+//  3. 덧셈식 출력하기.swift
+//  Algorithm
+//
+//  Created by 김동현 on 7/6/25.
+//
+
+import Foundation
+
+@main
+struct Main {
+    static func main() {
+        let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+        let (a, b) = (n[0], n[1])
+
+        print("\(a) + \(b) = \(a + b)")
+    }
+}
