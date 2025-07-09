e07cab582fcf63309463f8dfadb21a730476ec6c
indextrown <indextrown@gmail.com>
2025-07-09 19:59:07 +0900

    upload

--- 3. Programmers/Level0/3. 조건문/5. 각도기.swift
+++ 3. Programmers/Level0/3. 조건문/5. 각도기.swift
@@ -26,4 +26,20 @@ func solution(_ angle:Int) -> Int {
 }
 */
 
-s
+func solution(_ angle:Int) -> Int {
+    let result: Int
+    
+    if angle > 0 && angle < 90 {
+        result = 1
+    } else if angle == 90 {
+        result = 2
+    } else if angle > 90 && angle < 180 {
+        result = 3
+    } else if angle == 180 {
+        result = 4
+    } else {
+        result = 0
+    }
+    
+    return result
+}
