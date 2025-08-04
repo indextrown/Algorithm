b9dff56f3fef709712d6299416d21b9d956bd141
indextrown <indextrown@gmail.com>
2025-08-05 02:42:24 +0900

    update

--- 3. Programmers/12. 주식 가격/main.swift
+++ 3. Programmers/12. 주식 가격/main.swift
@@ -12,8 +12,7 @@ import Foundation
  2 <= price길이 <= 100,000 => 시간복잡도가 O(N)으로 접근해야 한다, 알고리즘이 떠오르지 않으면 O(n^2)으로 정확성 테스트 정보 확보하자
  */
 
-
-func solution(_ prices:[Int]) -> [Int] {
+func solution1(_ prices:[Int]) -> [Int] {
     var answer: [Int] = []
     
     for i in 0..<prices.count {
@@ -39,6 +38,57 @@ func solution(_ prices:[Int]) -> [Int] {
     return answer
 }
 
-print(solution([1, 2, 3, 2, 3]))
+// MARK: - O(n^2)
+func solution2(_ prices:[Int]) -> [Int] {
+    
+    /*
+     가격이 떨어지지 않은 시간을 구하는 문제인데
+     가격이 떨어지지 않은 횟수만 세고 떨어지는 시점에는 반복을 종료하지 않았다..
+     
+     var result: [Int] = []
+     for i in 0..<prices.count {
+         var count = 0
+         for j in i+1..<prices.count {
+             if prices[i] <= prices[j] {
+                 count += 1
+             }
+         }
+         result.append(count)
+     }
+     return result
+     */
+    
+    var result: [Int] = []
+    for i in 0..<prices.count {
+        var count = 0
+        for j in i+1..<prices.count {
+            count += 1
+            if prices[i] > prices[j] { break }
+        }
+        result.append(count)
+    }
+    
+    return result
+}
 
+// MARK: - O(n)
+func solution3(_ prices:[Int]) -> [Int] {
+    var stack: [Int] = []
+    var result = Array(repeating: 0, count: prices.count)
+    
+    for i in 0..<prices.count {
+        while !stack.isEmpty && prices[stack.last!] > prices[i] {
+            result[stack.last!] = i - stack.last!
+            stack.removeLast()
+        }
+        stack.append(i)
+    }
+    
+    for index in stack {
+        result[index] = prices.count - index - 1
+    }
+    
+    return result
+}
 
+print(solution3([1, 2, 3, 2, 3]))
