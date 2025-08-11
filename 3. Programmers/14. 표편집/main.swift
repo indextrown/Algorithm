117051fdac1f18b5d3518d37743cf9fe1dd17942
indextrown <indextrown@gmail.com>
2025-08-11 15:48:51 +0900

    update

--- 3. Programmers/14. 표편집/main.swift
+++ 3. Programmers/14. 표편집/main.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+func solution(_ n:Int, _ k:Int, _ cmd:[String]) -> String {
+    var stack: [Int] = [] // 삭제 보관
+    var row = k // 선택된 행
+    var rowCount = n
+    
+    for c in cmd {
+        let parts = c.split(separator: " ")
+        let command = parts[0]
+        let move = parts.count > 1 ? Int(parts[1])! : 0
+        
+        switch command {
+        case "U":
+            break
+        case "D":
+            row = min(rowCount - 1, row + move)
+        case "C":
+            break
+        case "D":
+            break
+        default : break
+        }
+    }
+    return ""
+}
+
+print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z"]))
+// print(solution(8, 2, ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]))
+
