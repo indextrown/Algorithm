e47eb676371b0c1d9cb3025cd7544bdfb90509dd
indextrown <indextrown@gmail.com>
2026-02-04 13:52:56 +0900

    20260204

---
+++ 2. Baekjoon/코딩테스트/6. 구현/c. 일곱난쟁이/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김동현 on 2/3/26.
+//
+
+import Foundation
---
+++ 2. Baekjoon/코딩테스트/6. 구현/f. 팰린드롬인지 확인하기/main.swift
@@ -0,0 +1,22 @@
+/*
+[회문]
+거꾸로 읽어도 똑바로 읽어도 똑같은 언어
+*/
+
+import Foundation
+
+freopen("input.txt", "r", stdin)
+
+let chars = readLine()!
+var result: Bool = true
+
+for i in 0..<chars.count/2 {
+
+    let left = chars.index(chars.startIndex, offsetBy: i)
+    let right = chars.index(chars.startIndex, offsetBy: chars.count - i - 1)
+
+    if chars[left] != chars[right] { result = false }
+}
+
+print(result ? 1 : 0)
+
